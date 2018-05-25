562fb795d35162c53af44182bd089c41638f9979
Jane Wang <yuexian.wang@gov.bc.ca>
2018-05-25 09:24:06 -0700

    Correct wind dataframe error in growing season section

--- R/03_analysis.R
+++ R/03_analysis.R
@@ -14,12 +14,6 @@
 library(dplyr) # for data analysis
 library(reshape2) # for long dataframes
 
-## classifying months to summer and winter climate seasons
-wxstn_df$Season <- NA
-wxstn_df[wxstn_df$months == "October" | wxstn_df$months == "November" | wxstn_df$months == "December"| wxstn_df$months == "January" |
-           wxstn_df$months == "February" | wxstn_df$months == "March"| wxstn_df$months == "April" , "Season"] <- "Winter"
-wxstn_df[wxstn_df$months == "May" | wxstn_df$months == "June"| wxstn_df$months == "July" | wxstn_df$months == "August" | wxstn_df$months == "September", "Season"] <- "Summer"
-
 ## calculating month average insolation
 wxstn_df <- wxstn_df %>%
   group_by(Site, months) %>%
@@ -36,15 +30,35 @@ df <- select(wxstn_df, c("Site", "Date", "years", "months", "dates", "Rain_sum",
 df_long <- melt(df, id.vars = c("Site", "Date", "dates", "months", "years"))
 levels(df_long$variable) <- c("Precipitation (mm)", "Pressure (mb)", "Temperature (degree C)", "Relative Humidity (%)", "Dew Point (degree C)")
 
+## classifying months to 4 seasons
+wxstn_df$seasons <- NA
+wxstn_df[wxstn_df$months == "December"| wxstn_df$months == "January" | wxstn_df$months == "February", "seasons"] <- "Winter"
+wxstn_df[wxstn_df$months == "March"| wxstn_df$months == "April" | wxstn_df$months == "May", "seasons"] <- "Spring"
+wxstn_df[wxstn_df$months == "June"| wxstn_df$months == "July" | wxstn_df$months == "August", "seasons"] <- "Summer"
+wxstn_df[wxstn_df$months == "September"| wxstn_df$months == "October" | wxstn_df$months == "November", "seasons"] <- "Fall"
+
+## classifying months to summer and winter growing seasons
+wxstn_df$gseason <- NA
+wxstn_df[wxstn_df$months == "October" | wxstn_df$months == "November" | wxstn_df$months == "December"| wxstn_df$months == "January" |
+          wxstn_df$months == "February" | wxstn_df$months == "March"| wxstn_df$months == "April" , "gseason"] <- "Winter"
+wxstn_df[wxstn_df$months == "May" | wxstn_df$months == "June"| wxstn_df$months == "July" | wxstn_df$months == "August" | wxstn_df$months == "September", "gseason"] <- "Summer"
+
 ## converting wind speed from m/s to km/h
 wind_df$WS <- wind_df$WS * 3.6
-
-## classifying months to summer and winter climate seasons
-wind_df$Season <- NA
-wind_df[wxstn_df$months == "October" | wxstn_df$months == "November" | wxstn_df$months == "December"| wxstn_df$months == "January" |
-           wxstn_df$months == "February" | wxstn_df$months == "March"| wxstn_df$months == "April" , "Season"] <- "Winter"
-wind_df[wxstn_df$months == "May" | wxstn_df$months == "June"| wxstn_df$months == "July" | wxstn_df$months == "August" | wxstn_df$months == "September", "Season"] <- "Summer"
 
 ## classifying wind speed into categories for windrose plot
 wind_df$WS <- cut(wind_df$WS, c(-Inf, 3, 6, 9, Inf))
 levels(wind_df$WS) <- rev(levels(wind_df$WS))
+
+## classifying months to 4 seasons
+wind_df$seasons <- NA
+wind_df[wind_df$months == "December"| wind_df$months == "January" | wind_df$months == "February", "seasons"] <- "Winter"
+wind_df[wind_df$months == "March"| wind_df$months == "April" | wind_df$months == "May", "seasons"] <- "Spring"
+wind_df[wind_df$months == "June"| wind_df$months == "July" | wind_df$months == "August", "seasons"] <- "Summer"
+wind_df[wind_df$months == "September"| wind_df$months == "October" | wind_df$months == "November", "seasons"] <- "Fall"
+
+## classifying months to summer and winter growing seasons
+wind_df$gseason <- NA
+wind_df[wind_df$months == "October" | wind_df$months == "November" | wind_df$months == "December"| wind_df$months == "January" |
+          wind_df$months == "February" | wind_df$months == "March"| wind_df$months == "April" , "gseason"] <- "Winter"
+wind_df[wind_df$months == "May" | wind_df$months == "June"| wind_df$months == "July" | wind_df$months == "August" | wind_df$months == "September", "gseason"] <- "Summer"
