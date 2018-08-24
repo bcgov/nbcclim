8b96960fc68b468a61780bb33751a6bc12e8ec8e
Jane <yuexian.wang@gov.bc.ca>
2018-08-24 13:53:02 -0700

    Delete unused code

--- R/shiny/www/js/bcgov.js
+++ R/shiny/www/js/bcgov.js
@@ -1,37 +1,4 @@
 $(document).ready(function() {
-  // Explore population
-  $('.explore-population').on('click', function() {
-    $('a[data-value="Census Topics"]').click();
-    setTimeout(findSpan, 500, "Population");
-  });
-
-  // Explore housing
-  $('.explore-housing').on('click', function() {
-    $('a[data-value="Census Topics"]').click();
-    setTimeout(findSpan, 500, "Housing");
-  });
-
-  // Explore mobility
-  $('.explore-mobility').on('click', function() {
-    $('a[data-value="Census Topics"]').click();
-    setTimeout(findSpan, 500, "Mobility");
-  });
-
-  // Explore STIR
-  $('.explore-stir').on('click', function() {
-    $('a[data-value="Census Topics"]').click();
-    setTimeout(findSpan, 500, "STIR");
-  });
-
-  // Explore PTT
-  $('.explore-ptt').on('click', function() {
-    $('a[data-value="Property Transfer Tax"]').click();
-  });
-
-  // Footer about link
-  $('.about-project').on('click', function() {
-    $('a[data-value="Project background"]').click();
-  });
 
   // Back to top
 	$(".back-to-top").on("click", function(e) {
