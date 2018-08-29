53a33f14c971a5f631d28f6fa8f26fbd02dd5dd5
Jane Wang <yuexian.wang@gov.bc.ca>
2018-08-29 16:40:14 -0700

    Better comments

--- R/02_clean.R
+++ R/02_clean.R
@@ -27,9 +27,11 @@ wind_df$Day <- as.Date(wind_df$Day, "%Y-%m-%d")
 wind_df$months <- months(wind_df$Day, abbreviate = TRUE)
 
 ## stations data frame
+## real time stations
 rt <- subset(wxstn_df,  Site == "Blackhawk" | Site == "Canoe" | Site == "Hourglass" | Site == "Hudson Bay Mountain" |
                Site == "McBride Peak" | Site == "Nonda" | Site == "Pink Mountain", select = c(Site, Longitude, Latitude, Elevation))
 rt <- subset(rt, !duplicated(rt$Site))
 
+## long-term weather stations
 wxstn_sites <- subset(wxstn_df, select = c(Site, Longitude, Latitude, Elevation))
 wxstn_sites <- subset(wxstn_sites, !duplicated(wxstn_sites$Site))
