3cd7b27845d19fad2c74e5e158875b6cdb763a9d
Jane Wang <yuexian.wang@gov.bc.ca>
2018-12-05 15:18:24 -0800

    Add more comments to the code

--- R/00_prep.R
+++ R/00_prep.R
@@ -29,11 +29,20 @@ for (i in 1:length(updates)) {
   df <- read_csv(paste0("G:/!Workgrp/Research/JWang/WxUpdates_Hly2018/csv/", updates[i]))
   df_orig <- read_csv(paste0("G:/!Workgrp/Research/JWang/ClimateData/WxStns/csv/hourly/", originals[i]))
 
+  ## if read_csv doesn't parse correctly, use read.csv for above and uncomment the folloing
+  ########
+  # df$Date.and.Time..PST <- as.POSIXct(df$Date.and.Time..PST, "%Y-%m-%d %H:%M:%S")
+  # df_orig$Date <- as.POSIXct(df_orig$Date, "%Y-%m-%d %H:%M:%S")
+  ########
+
+  ## look for weird names that cannot be detected by the grep() function below
   print(updates[i])
   print(names(df))
 
+  ## only select wind-relevant columns
   df_orig <- select(df_orig, Site, Date, Day, WS, WD)
 
+  ## cleaning input updated dataframe
   df_out <- df %>%
     select(grep("Date|Day|Wind.Speed|Wind.speed|wind.speed|Wind.Direction|Wind.direction|wind.direction",
                 colnames(df))) %>%
@@ -42,10 +51,14 @@ for (i in 1:length(updates)) {
            "WS" = names(df[grep("Wind.Speed|Wind.speed|wind.speed", colnames(df))]),
            "WD" = names(df[grep("Wind.Direction|Wind.direction|wind.direction", colnames(df))]))
 
+  ## check if the colnames are correctly renamed. Compare among two dataframes for number of
+  ## NAs and dimensions
   print("summary of cleaned df")
   print(summary(df_out))
   print(summary(df_orig))
 
+  ## if the join for some reason creates new rows that shouldn't be, examine dataframes
+  ## if still problematic can directly output df_out if start date is consistent with df_orig
   df_out <- full_join(df_orig, df_out)
 
   ## filling in all rows with Site names
@@ -53,11 +66,14 @@ for (i in 1:length(updates)) {
 
   df_out <- select(df_out, Site, Date, Day, WS, WD)
 
+  ## look for number of NAs, if they are consistent with inputs'. Look for start and end date
+  ## and see if everything updated. Also detect for mutating rows (shouldn't be any for updates)
   print("summary of final df")
   print(summary(df_out))
   print(head(df_out))
   print(head(df_orig))
 
+  ## stop to examine before output for every dataset
   browser()
   write_csv(df_out, paste0("G:/!Workgrp/Research/JWang/ClimateData/WxStns/csv/hourly/updates/", updates[i]))
 
